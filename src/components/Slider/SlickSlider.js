16f02f8ffc52e1e94826492705a9d629f0f6f549
ijazali <ijazalideveloper@gmail.com>
2023-04-11 16:10:03 +0500

    slider and button compoenent index file added

---
+++ src/components/Slider/SlickSlider.js
@@ -0,0 +1,53 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+import * as styles from "./slickSlider.module.scss";
+import React from "react";
+
+const defaultSettings = {
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
+const SlickSlider = ({ className = "", children, customSliderSettings = {}, ...props }) => {
+  const sliderDefaultPlusCustomSettings = {
+    ...defaultSettings,
+    ...customSliderSettings,
+  };
+
+  return (
+    <Slider className={`${className} ${styles.slickSlider}`} {...sliderDefaultPlusCustomSettings} {...props}>
+      {children}
+    </Slider>
+  );
+};
+
+export default SlickSlider;
